147b032354bc700fd9ae5b886b0e885a25ad3c6a
Анна Полякова <Sova040888@yandex.ru>
2021-06-25 17:20:28 +0300

    pycodestyle

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -9,12 +9,12 @@ DATABASES = {
     }
 }
 
-INSTALLED_APPS += [
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # указываем директорию, в которую будут складываться файлы писем
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
