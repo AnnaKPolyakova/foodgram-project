c4a5fa5ec1d0d78ac6c396fac8e6a41c4e4a96ce
Анна Полякова <Sova040888@yandex.ru>
2021-07-12 21:36:37 +0300

    deploy

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,12 +1,10 @@
 import os
 from pathlib import Path
 
-# from dotenv import load_dotenv
-
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = "django-insecure-$mozfaf0chjr_ea32k_!$1otw39b2yk1v)xc6d&p2a*@b(-f$"
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = "django-insecure-$mozfaf0chjr_ea32k_!$1otw39b2yk1v)xc6d&p2a*@b(-f$"
 
 ALLOWED_HOSTS = [
     '*',
