cc4754edcc78ffe144c2426a2e19337555b66218
Анна Полякова <Sova040888@yandex.ru>
2021-07-15 17:50:54 +0300

    add send mess

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-# from django.core.mail import EmailMessage
-# from django.template.loader import render_to_string
+from django.conf.global_settings import EMAIL_HOST_USER
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -12,22 +13,24 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
-    #
-    # def post(self, request, *args, **kwargs):
-    #     form = self.get_form()
-    #     if form.is_valid():
-    #         html_message = render_to_string(
-    #             "mail.html",
-    #             {
-    #                 "login": form.cleaned_data["username"],
-    #                 "password": form.cleaned_data["password1"],
-    #             },
-    #         )
-    #         message = EmailMessage(
-    #             SUBJECT, html_message, to=[form.cleaned_data["email"]]
-    #         )
-    #         message.content_subtype = "html"
-    #         message.send()
-    #         return self.form_valid(form)
-    #     else:
-    #         return self.form_invalid(form)
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            html_message = render_to_string(
+                "mail.html",
+                {
+                    "login": form.cleaned_data["username"],
+                    "password": form.cleaned_data["password1"],
+                },
+            )
+            message = EmailMessage(
+                SUBJECT, html_message,
+                from_email=EMAIL_HOST_USER,
+                to=[form.cleaned_data["email"]]
+            )
+            message.content_subtype = "html"
+            message.send()
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
