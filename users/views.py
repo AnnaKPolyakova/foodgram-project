7aa3183c32faf1d42d2a8186423f30fb23a19dbc
Анна Полякова <Sova040888@yandex.ru>
2021-07-15 17:41:38 +0300

    add send mess

--- users/views.py
+++ users/views.py
@@ -12,22 +12,22 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
-
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            html_message = render_to_string(
-                "mail.html",
-                {
-                    "login": form.cleaned_data["username"],
-                    "password": form.cleaned_data["password1"],
-                },
-            )
-            message = EmailMessage(
-                SUBJECT, html_message, to=[form.cleaned_data["email"]]
-            )
-            message.content_subtype = "html"
-            message.send()
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+    #
+    # def post(self, request, *args, **kwargs):
+    #     form = self.get_form()
+    #     if form.is_valid():
+    #         html_message = render_to_string(
+    #             "mail.html",
+    #             {
+    #                 "login": form.cleaned_data["username"],
+    #                 "password": form.cleaned_data["password1"],
+    #             },
+    #         )
+    #         message = EmailMessage(
+    #             SUBJECT, html_message, to=[form.cleaned_data["email"]]
+    #         )
+    #         message.content_subtype = "html"
+    #         message.send()
+    #         return self.form_valid(form)
+    #     else:
+    #         return self.form_invalid(form)
