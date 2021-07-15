e2c6a5623ff570bcc4dd01aa547a2547fde24167
Анна Полякова <Sova040888@yandex.ru>
2021-07-15 17:30:38 +0300

    add send mess

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
+from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.core.mail import EmailMessage
 
 from .forms import CreationForm
 
